28b1289168adf6e891a28d9ca06799c9be89f3a6
Simon Sorg <simon.sorg@student.hpi.de>
2024-06-06 15:48:41 +0200

    fix: use test_suite_sql_eval

--- testing/mixtral/ehrsql.py
+++ testing/mixtral/ehrsql.py
@@ -20,14 +20,8 @@ from transformers_cfg.switches import (
 from tempfile import NamedTemporaryFile
 import tqdm
 from dotenv import load_dotenv
-import re
-import sys
 
-import sqlite3
-import numpy as np
-import multiprocessing as mp
-
-from func_timeout import func_timeout, FunctionTimedOut
+from test_suite_sql_eval.evaluation import evaluate, build_foreign_key_map_from_json
 
 load_dotenv()
 
@@ -391,158 +385,31 @@ def generate_one(
     )
 
 
-
-def post_process_sql(query,
-                     current_time="2105-12-31 23:59:00",
-                     precomputed_dict={
-                         'temperature': (35.5, 38.1),
-                         'sao2': (95.0, 100.0),
-                         'heart rate': (60.0, 100.0),
-                         'respiration': (12.0, 18.0),
-                         'systolic bp': (90.0, 120.0),
-                         'diastolic bp': (60.0, 90.0),
-                         'mean bp': (60.0, 110.0)
-                     }):
-    query = query.lower()
-    if "current_time" in query:
-        query = query.replace("current_time", f"'{current_time}'")
-    if re.search('[ \n]+([a-zA-Z0-9_]+_lower)', query) and re.search('[ \n]+([a-zA-Z0-9_]+_upper)', query):
-        vital_lower_expr = re.findall('[ \n]+([a-zA-Z0-9_]+_lower)', query)[0]
-        vital_upper_expr = re.findall('[ \n]+([a-zA-Z0-9_]+_upper)', query)[0]
-        vital_name_list = list(
-            set(re.findall('([a-zA-Z0-9_]+)_lower', vital_lower_expr) + re.findall('([a-zA-Z0-9_]+)_upper',
-                                                                                   vital_upper_expr)))
-        if len(vital_name_list) == 1:
-            processed_vital_name = vital_name_list[0].replace('_', ' ')
-            if processed_vital_name in precomputed_dict:
-                vital_range = precomputed_dict[processed_vital_name]
-                query = query.replace(vital_lower_expr, f"{vital_range[0]}").replace(vital_upper_expr,
-                                                                                     f"{vital_range[1]}")
-    query = query.replace("''", "'").replace('< =', '<=')
-    query = query.replace("%y", "%Y").replace('%j', '%J')
-    query = query.replace("'now'", f"'{current_time}'")
-    return query
-
-
-def process_answer(ans):
-    return str(sorted([str(ret) for ret in ans[:100]]))  # check only up to 100th record
-
-
-def execute(sql, db_path):
-    print("Executing sql")
-    con = sqlite3.connect(db_path)
-    cur = con.cursor()
-    result = cur.execute(sql).fetchall()
-    con.close()
-    return result
-
-
-def execute_wrapper(sql, timeout, db_path, tag, skip_indicator='null'):
-    if sql != skip_indicator:
-        try:
-            result = func_timeout(timeout, execute, args=(sql, db_path))
-        except KeyboardInterrupt:
-            sys.exit(0)
-        except FunctionTimedOut:
-            result = [(f'timeout_{tag}',)]
-        except Exception as e:
-            print(e)
-            result = [(f'error_{tag}',)]  # possibly len(query) > 512 or not executable
-        result = process_answer(result)
-    else:
-        result = skip_indicator
-    return result
-
-
-def execute_query(sql1, sql2, timeout, db_path, data_idx=None):
-    '''
-    Execute the query. Time out if it exceeds {args.timeout} seconds
-    '''
-    result1 = execute_wrapper(sql1, timeout, db_path, tag='real')
-    result2 = execute_wrapper(sql2, timeout, db_path, tag='pred')
-    result = {'data_idx': data_idx, 'real': result1, 'pred': result2}
-    return result
-
-
-def execute_query_distributed(real, pred, timeout, db_path, num_workers):
-    exec_result = []
-
-    def result_tracker(result):
-        exec_result.append(result)
-
-    pool = mp.Pool(processes=num_workers)
-    for data_idx, (sql1, sql2) in enumerate(zip(real, pred)):
-        pool.apply_async(execute_query, args=(sql1, sql2, timeout, db_path, data_idx), callback=result_tracker)
-    pool.close()
-    pool.join()
-
-    return exec_result
-
-
-def evaluate(gold_file, pred_file, db_path):
-    num_workers = mp.cpu_count()
-    with open(gold_file, 'r') as f:
-        data = json.load(f)
-    with open(pred_file, 'r') as f:
-        pred = json.load(f)
-
-    data_id = []
-    query_real = []
-    query_pred = []
-    for line in data:
-        id_ = line['id']
-        if id_ not in pred:
-            continue
-        else:
-            print(id_)
-        data_id.append(id_)
-        real = post_process_sql(line['query'], current_time="2105-12-31 23:59:00")
-        query_real.append(real)
-        if id_ in pred:
-            query_pred.append(post_process_sql(pred[id_], current_time="2105-12-31 23:59:00"))
-        else:
-            query_pred.append('n/a')
-
-    exec_real = []
-    exec_pred = []
-    if num_workers > 1:
-        exec_result = execute_query_distributed(query_real, query_pred, 60.0, db_path, num_workers)
-        indices = []
-        for ret in exec_result:
-            exec_real.append(ret['real'])
-            exec_pred.append(ret['pred'])
-            indices.append(ret['data_idx'])
-        exec_real = np.array(exec_real)[np.argsort(indices)]
-        exec_pred = np.array(exec_pred)[np.argsort(indices)]
-    else:
-        for sql1, sql2 in zip(query_real, query_pred):
-            ret = execute_query(sql1, sql2, 60.0, db_path)
-            exec_real.append(ret['real'])
-            exec_pred.append(ret['pred'])
-        exec_real = np.array(exec_real)
-        exec_pred = np.array(exec_pred)
-
-    correct = 0
-
-    for idx in range(len(exec_real)):
-        ans_real, ans_pred = exec_real[idx], exec_pred[idx]
-        if ans_pred == ans_real:
-            correct += 1
-
-    out_eval = {'execution_accuracy', round(correct / len(exec_real))}
-    return out_eval
+def postprocess_sql(sql):
+    # Remove comments
+    lines = sql.split("\n")
+    # Remove rest of line after --
+    lines = [line.split("--")[0] for line in lines]
+    # Remove empty lines and strip
+    lines = [line.strip() for line in lines if line.strip()]
+    # Substitute \n
+    sql = " ".join(lines)
+    return sql
 
 
 # Path environment variables
 EHRSQL_PATH = os.environ.get("EHRSQL_PATH", None)
-DB_PATH = os.environ.get("DB_PATH", None)
+TABLES_JSON_PATH = os.environ.get("TABLES_JSON_PATH", None)
+DB_DIR = os.environ.get("DB_DIR", None)
 GOLD_FILE = os.environ.get("GOLD_FILE", None)
 GRAMMAR_PATH = os.environ.get("GRAMMAR_PATH", None)
 
 # Check if all paths are set
 if not EHRSQL_PATH:
     raise ValueError("EHRSQL_PATH not set")
-if not DB_PATH:
+if not TABLES_JSON_PATH:
+    raise ValueError("TABLES_JSON_PATH not set")
+if not DB_DIR:
     raise ValueError("DB_PATH not set")
 if not GOLD_FILE:
     raise ValueError("GOLD_FILE not set")
@@ -550,8 +417,8 @@ if not GRAMMAR_PATH:
     raise ValueError("GRAMMAR_PATH not set")
 
 print("EHRSQL_PATH:", EHRSQL_PATH)
-print("DB_PATH:", DB_PATH)
-print("GOLD_FILE:", GOLD_FILE)
+print("TABLES_JSON_PATH:", TABLES_JSON_PATH)
+print("DB_DIR:", DB_DIR)
 print("GRAMMAR_PATH:", GRAMMAR_PATH)
 
 
@@ -573,7 +440,7 @@ def main():
         # Filter for possible
         questions = list(filter(lambda x: not x["is_impossible"], questions))
 
-    results = {}
+    results = []
     total_fallbacks = 0
     total_switches = 0
     total_switches_wo_fallback = 0
@@ -595,14 +462,23 @@ def main():
         total_fallbacks += fallbacks
         total_switches += switches
         total_switches_wo_fallback += switches_wo_fallback
-        results[question["id"]] = result
+        result = postprocess_sql(result)
+        results.append(result)
 
     # Dump to temp file
-    with NamedTemporaryFile("w", suffix=".json") as f:
+    with NamedTemporaryFile("w", suffix=".sql") as f:
         f.write("\n".join(results))
         f.flush()
-        eval_res = evaluate(GOLD_FILE, f.name, DB_PATH)
-        print(eval_res)
+        # Convert ehrsql json to sql
+        with NamedTemporaryFile("w", suffix=".sql") as f_gold:
+            gold_results = [
+                postprocess_sql(question["query"]) for question in questions
+            ]
+            f_gold.write("\n".join(gold_results))
+            kmaps = build_foreign_key_map_from_json(TABLES_JSON_PATH)
+            eval_res = evaluate(f_gold.name, f.name, DB_DIR, "all", kmaps, plug_value=False, keep_distinct=False,
+                                progress_bar_for_each_datapoint=False)
+            print(eval_res)
     print("Total fallbacks", total_fallbacks)
     print("Total switches", total_switches)
     print("Total switches without fallback", total_switches_wo_fallback)
